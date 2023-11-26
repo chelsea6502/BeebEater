90fc79de16e7846c56014561ca62e1b8d1166cea
Chelsea Wilkinson <mail@chelseawilkinson.me>
2023-11-26 21:39:27 +1100

    Bump version number

--- BeebEater.asm
+++ BeebEater.asm
@@ -1,4 +1,4 @@
-; BeebEater v0.4.2 - BBC BASIC port for the Ben Eater 6502.
+; BeebEater v1.0 - BBC BASIC port for the Ben Eater 6502.
 ; by Chelsea Wilkinson (chelsea6502)
 ; https://github.com/chelsea6502/BeebEater
 
